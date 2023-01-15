734ec412569d0059ce2a2defc1670b98ec312c7a
abass-dev <abassthedev@gmail.com>
2023-01-15 20:18:46 +0100

    hack commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
             <div className={styles.buttons}>
               <Link
                 className="button button--secondary button--lg"
-                to="/docs/hello"
+                to="/docs/intro"
               >
                 NigaPHP in 5min ⏱️
               </Link>
